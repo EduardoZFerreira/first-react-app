f5d5dc561c85c37eec398e42710713f20e87006f
eduardo.zekl@gmail.com <eduardo.zekl@gmail.com>
2024-10-21 20:42:00 -0300

    Adding a listgroup

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,24 @@
+function ListGroup() {
+  let items = ["New York", "San Francisco", "London", "Paris"];
+  items = [];
+
+  const getMessage = () => {
+    return items.length === 0 && <p>No item found</p>;
+  };
+
+  return (
+    <>
+      <h1>List</h1>
+      {getMessage()}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li className="list-group-item" key={item}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
