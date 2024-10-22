b89690d07f0ffb80326c0889ece6862d8768d9bf
eduardo.zekl@gmail.com <eduardo.zekl@gmail.com>
2024-10-21 21:00:27 -0300

    Adding state management

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,15 @@
-import { MouseEvent } from "react";
+import { MouseEvent, useState } from "react";
 
 function ListGroup() {
   let items = ["New York", "San Francisco", "London", "Paris"];
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   const getMessage = () => {
     return items.length === 0 && <p>No item found</p>;
   };
 
-  const handleClick = (event: MouseEvent) => {
-    console.log(event);
+  const handleClick = (event: MouseEvent, index: any) => {
+    setSelectedIndex(index);
   };
 
   return (
@@ -16,8 +17,18 @@ function ListGroup() {
       <h1>List</h1>
       {getMessage()}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+        {items.map((item, index) => (
+          <li
+            className={
+              index === selectedIndex
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={(event) => {
+              handleClick(event, index);
+            }}
+          >
             {item}
           </li>
         ))}
